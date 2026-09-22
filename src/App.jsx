88797a6fe916bedb74593dc9943db74dc88777dd
Saul2412 <Saul2412@users.noreply.github.com>
2026-09-22 09:16:02 -0600

    Revert to JSX files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hola Mundo</h1>
+      <p>Bienvenido a CitasReact</p>
+    </div>
+  )
+}
+
+export default App
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { StrictMode, createElement } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.js'
-
-createRoot(document.getElementById('root')).render(
-  createElement(StrictMode, null, createElement(App)),
-)
